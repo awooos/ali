2b239fff511b8014bae64e0d5a39533e9ee3bf56
Ellen Marie Dash <me@duckie.co>
2019-02-16 17:55:54 -0500

    add stdio.h and stdarg.h

---
+++ include/stdio.h
@@ -0,0 +1,20 @@
+#ifndef ALI__STDIO_H
+#define ALI__STDIO_H
+
+#include <stdarg.h>
+#include <stddef.h>
+
+//#define stdin   0
+//#define stdout  1
+//#define stderr  2
+
+#define EOF (-1)
+
+int printf(const char *format, ...);
+int putchar(int);
+int puts(const char *);
+int vprintf(const char *format, va_list args);
+int vsnprintf(char *str, size_t size, const char *format, va_list args);
+int vsprintf(char *str, const char *format, va_list args);
+
+#endif
