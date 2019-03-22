3c03de1ebefe95ff00d026b0128b5ab6eb0b4268
Ellen Marie Dash <me@duckie.co>
2019-03-21 21:37:04 -0400

    Fix typo wrt which numbers are negative; set buffer to a known value before testing, for easier debugging.

--- src/printf/main_test.c
+++ src/printf/main_test.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 size_t test_ali_printf_assertions(void)
 {
@@ -10,16 +11,24 @@ size_t test_ali_printf_assertions(void)
     char str[20];
     int ret;
 
+    // Set str to the letter "x" repeated 19 times.
+    memset(str, (int)'x', 19);
+    str[19] = 0;
+
     ret = snprintf(str, 20, "bork bork\n");
     TEST_ASSERT(ret > 0);
     TEST_ASSERT(strncmp((const char*)str, "bork bork\n", 20) == 0);
 
-    ret = snprintf(str, 20, "%s %i %u %s", "foo", -3, 4, "bar");
+    // Set str to the letter "x" repeated 19 times.
+    memset(str, (int)'x', 19);
+    str[19] = 0;
+
+    ret = snprintf(str, 20, "%s %i %u %s", "foo", 3, -4, "bar");
     TEST_ASSERT(ret > 0);
     puts("\n\nstr='");
     puts(str);
     puts("'\n\n");
-    TEST_ASSERT(strncmp((const char*)str, "foo -3 4 bar", 20) == 0);
+    TEST_ASSERT(strncmp((const char*)str, "foo 3 -4 bar", 20) == 0);
 
     TEST_ASSERTIONS_RETURN();
 }
